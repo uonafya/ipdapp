8bf3241f67837c6e0add9fcd664ed883fb551eb7
ningosi <nicotwendelee@gmail.com>
2020-09-18 11:26:39 +0300

    Adjustments to include correct activator for the IPD module

--- api/src/main/java/org/openmrs/module/ipdapp/IpdActivator.java
+++ api/src/main/java/org/openmrs/module/ipdapp/IpdActivator.java
@@ -21,7 +21,7 @@ import org.openmrs.module.ModuleActivator;
 /**
  * This class contains the logic that is run every time this module is either started or stopped.
  */
-public class ipdActivator implements ModuleActivator {
+public class IpdActivator implements ModuleActivator {
 	
 	protected Log log = LogFactory.getLog(getClass());
 		
